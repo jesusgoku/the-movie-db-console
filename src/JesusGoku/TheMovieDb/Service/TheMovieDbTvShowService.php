0a90f19cc78f98206e5a04ecbc83fac3e6331ef1
Jesus Urrutia <jesus.urrutia@gmail.com>
2015-04-29 17:58:50 -0300

    Added draft of The Movie DB Services

---
+++ src/JesusGoku/TheMovieDb/Service/TheMovieDbTvShowService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace JesusGoku\TheMovieDb\Service;
+
+
+/**
+ * Class TheMovieDbTvShowService
+ * @package JesusGoku\TheMovieDb\Service
+ *
+ * @author Jesus Urrutia <jesus.urrutia@gmail.com>
+ */
+class TheMovieDbTvShowService implements TvShowServiceInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function search($tvShowName, $language = null)
+    {
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getEpisode($episodeId, $language = null)
+    {
+
+    }
+}
