717d52daff92fb9104d2b3b519e080ae14790b98
Jesus Urrutia <jesus.urrutia@gmail.com>
2015-06-18 02:20:37 -0300

    Added doc comment to config var

--- src/JesusGoku/TheMovieDb/Command/TvShowEpisodeCommand.php
+++ src/JesusGoku/TheMovieDb/Command/TvShowEpisodeCommand.php
@@ -18,6 +18,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class TvShowEpisodeCommand extends Command
 {
+    /** @var array */
     private $config;
 
     protected function configure()
